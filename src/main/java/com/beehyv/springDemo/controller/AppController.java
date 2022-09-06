8204c96854493816e04f642a726f5822a50af326
anubhav0101 <anubhavbansal0101@gmail.com>
2022-09-06 16:13:18 +0530

    Returning json value instead of a string

--- src/main/java/com/beehyv/springDemo/controller/AppController.java
+++ src/main/java/com/beehyv/springDemo/controller/AppController.java
@@ -1,19 +1,31 @@
 package com.beehyv.springDemo.controller;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.beehyv.springDemo.response.HelloResposne;
 
 @RestController
 public class AppController {
 	@GetMapping("/printHello/{name}")
-	public String print(@PathVariable String name){
+	public String print(@PathVariable String name) {
 		return " Hello " + name;
 	}
 
-	
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/helloWorld")
+	public ResponseEntity<?> helloWorld(@RequestParam String print) {
+		HelloResposne res = new HelloResposne();
+		res.setId(counter.incrementAndGet());
+		res.setPrint("hello " + print);
+		return new ResponseEntity<HelloResposne>(res, HttpStatus.ACCEPTED);
+	}
 
 }
-
-
-
---
+++ src/main/java/com/beehyv/springDemo/response/HelloResposne.java
@@ -0,0 +1,19 @@
+package com.beehyv.springDemo.response;
+
+public class HelloResposne {
+	private Long id;
+	private String print;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getPrint() {
+		return print;
+	}
+	public void setPrint(String print) {
+		this.print = print;
+	}
+
+}
